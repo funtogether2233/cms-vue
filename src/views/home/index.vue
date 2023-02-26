49c77b7b290736d31a9f58ece5c89ff208b6a7e9
LiChen <2446432344@qq.com>
2023-02-26 14:25:42 +0800

    feat: 增加退出登录按钮

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="home">home</div>
+  <button @click="logout">退出登录</button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { LOGIN_TOKEN } from '@/global/constants';
+import router from '@/router';
+import { localCache } from '@/utils/cache';
+
+// 退出登录
+function logout() {
+  // 删除token
+  localCache.removeCache(LOGIN_TOKEN);
+
+  // 跳回login界面
+  router.push('/login');
+}
+</script>
 
 <style lang="less" scoped></style>
